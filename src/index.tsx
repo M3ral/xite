1b5491e5b3e877b171bf92b9a598884978eb2978
Meral <meral@kinder.world>
2021-08-17 00:43:25 +0200

    Small style changes

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.Fragment>
+    <CssBaseline/>
+    <App/>
+  </React.Fragment>,
   document.getElementById('root')
 );
 
